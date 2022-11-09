e6754bfe0a3860c220d3ba0e66e33d08af9a9c67
tamura_kazuma <kazuma.tamura.6n@stu.hosei.ac.jp>
2022-11-09 16:51:23 +0900

    format state

--- reversi/sample.py
+++ reversi/sample.py
@@ -1,9 +1,55 @@
-from cppMCTS import *
+# from cppMCTS import *
+from cppState import *
+
+# xs = [1.0, 0.0, 0.5]
+
+# m = MCTS(1)
+# print(m)
+# xs = m.boltzman(xs)
+# print(xs)
+
+# ====================
+# リバーシ
+# ====================
+
+# パッケージのインポート
+import random
+from settings import SQUARE, default_ratio_box
 
 
-xs = [1.0, 0.0, 0.5]
+def random_action(state):
+    """ランダムで行動選択"""
+    legal_actions = state.legal_actions()
+    return legal_actions[random.randint(0, len(legal_actions)-1)]
 
-m = MCTS(1)
-print(m)
-xs = m.boltzman(xs)
-print(xs)
+
+def print_reversi(state):
+    """文字列表示"""
+    ox = ('o', 'x') if state.is_first_player() else ('x', 'o')
+    str = ''
+    for i in range(SQUARE * SQUARE):
+        if state.pieces[i] == 1:
+            str += ox[0]
+        elif state.enemy_pieces[i] == 1:
+            str += ox[1]
+        else:
+            str += '-'
+        if i % SQUARE == (SQUARE - 1):
+            str += '\n'
+
+
+if __name__ == '__main__':
+    # 状態の生成
+    state = State()
+
+    # ゲーム終了までのループ
+    while True:
+        # ゲーム終了時
+        if state.is_done():
+            break
+
+        # 次の状態の取得
+        state = state.next(random_action(state))
+        # 文字列表示
+        print_reversi(state)
+        print()
--- reversi/state.cpp
+++ reversi/state.cpp
@@ -17,6 +17,7 @@ private:
     int depth = 0;
     vector<vector<int>> dxy = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
     bool pass_end = false;
+
 public:
     State(vector<int> pieces, vector<int> enemy_pieces, vector<int> ratio_box, int depth = 0)
     {
@@ -25,15 +26,16 @@ public:
         this->ratio_box = ratio_box;
         this->depth = depth;
 
-	if (this->pieces.size() == 0 || this->enemy_pieces.size() == 0){
-		vector<int> pieces = vector<int>(16, 0);
-    		vector<int> enemy_pieces = vector<int>(16, 0);
-    		vector<int> ratio_box = vector<int>(16, 100);
-		this->pieces[center_idx - balance_idx - 1] = 1;
-    		this->pieces[center_idx + balance_idx] = 1;
-    		this->enemy_pieces[center_idx - balance_idx] = 1;
-    		this->enemy_pieces[center_idx + balance_idx - 1] = 1;
-	}
+        if (this->pieces.size() == 0 || this->enemy_pieces.size() == 0)
+        {
+            vector<int> pieces = vector<int>(16, 0);
+            vector<int> enemy_pieces = vector<int>(16, 0);
+            vector<int> ratio_box = vector<int>(16, 100);
+            this->pieces[center_idx - balance_idx - 1] = 1;
+            this->pieces[center_idx + balance_idx] = 1;
+            this->enemy_pieces[center_idx - balance_idx] = 1;
+            this->enemy_pieces[center_idx + balance_idx - 1] = 1;
+        }
     }
 
     int piece_count(vector<int> pieces)
@@ -89,7 +91,7 @@ public:
     {
         State state = State(pieces, enemy_pieces, ratio_box, depth + 1);
         vector<int> pass_vec = {16};
-	if (action == 16 && state.legal_actions() == pass_vec)
+        if (action == 16 && state.legal_actions() == pass_vec)
         {
             pass_end = true;
         }
