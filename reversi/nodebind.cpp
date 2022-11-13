65d8eb5b56c9e817cebdcbb20ec0ad4ebb2937cc
tomru112345 <kazt23@gmail.com>
2022-11-13 21:59:49 +0900

    fix statebind32

--- reversi/nodebind.cpp
+++ reversi/nodebind.cpp
@@ -13,7 +13,7 @@ PYBIND11_MODULE(cppNode, m)
 {
     m.def("predict", &predict);
     pybind11::class_<Node>(m, "Node")
-        .def(pybind11::init<KerasModel, State, int>())
+        .def(pybind11::init<auto, State, int>())
         .def("evaluate", &Node::evaluate)
         .def("nodes_to_scores", &Node::nodes_to_scores)
         .def("next_child_node", &Node::next_child_node);
