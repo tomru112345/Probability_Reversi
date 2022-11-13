49f56a55f46b444b1bcf79732c3b1f9a2dffca2a
tomru112345 <kazt23@gmail.com>
2022-11-13 17:29:12 +0900

    add pypredict2

--- reversi/nodebind.cpp
+++ reversi/nodebind.cpp
@@ -1,6 +1,5 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include "keras_model.h"
 #include "node.cpp"
 #include "state.cpp"
 
@@ -11,7 +10,7 @@ PYBIND11_MODULE(cppNode, m)
     m.def("predict", &predict)
     .def("nodes_to_scores", &nodes_to_scores);
     pybind11::class_<Node>(m, "Node")
-        .def(pybind11::init<KerasModel, State, int>())
+        .def(pybind11::init<auto, State, int>())
         .def("evaluate", &Node::evaluate)
         .def("next_child_node", &Node::next_child_node);
 }
