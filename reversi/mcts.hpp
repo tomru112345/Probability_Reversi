3a7dae789e22e98bf29b1ae7c37445d040958d4e
tomru112345 <kazt23@gmail.com>
2022-11-13 22:52:57 +0900

    fix statebind37

--- reversi/mcts.hpp
+++ reversi/mcts.hpp
@@ -26,6 +26,106 @@ struct result_t predict(auto model, State state)
     return result;
 }
 
+class Node
+{
+private:
+public:
+    State state;
+    int p = 0;
+    int w = 0;
+    int n = 0;
+    vector<Node> child_nodes;
+    Node(State s, int np)
+    {
+        state = s;
+        p = np;
+        vector<Node> child_nodes;
+    }
+
+    vector<float> nodes_to_scores(vector<Node> nodes)
+    {
+        vector<float> scores;
+        for (int i = 0; i < nodes.size(); i++)
+        {
+            scores.push_back(nodes.at(i).n);
+        }
+        return scores;
+    }
+
+    float evaluate(auto model)
+    {
+        float value = 0;
+        if (this->state.is_done())
+        {
+            if (this->state.is_lose())
+            {
+                value = -1;
+            }
+            else
+            {
+                value = 0;
+            }
+            this->w += value;
+            this->n += 1;
+            return value;
+        }
+
+        if (this->child_nodes.empty())
+        {
+            auto result = predict(model, this->state);
+            vector<int> policies = result.p;
+            int value = result.v;
+            this->w += value;
+            this->n += 1;
+
+            for (int i = 0; i < policies.size(); i++)
+            {
+                this->child_nodes.push_back(Node(this->state.next(this->state.legal_actions().at(i)), policies.at(i)));
+            }
+            return value;
+        }
+        else
+        {
+            value -= this->next_child_node().evaluate(model);
+            this->w += value;
+            this->n += 1;
+            return value;
+        }
+    }
+
+    Node next_child_node()
+    {
+        float C_PUCT = 1.0;
+        vector<float> scores = nodes_to_scores(this->child_nodes);
+        float t = accumulate(scores.begin(), scores.end(), 0.0);
+
+        vector<float> pucb_values;
+        for (int i = 0; i < this->child_nodes.size(); i++)
+        {
+            if (this->child_nodes.at(i).n == 0)
+            {
+                return this->child_nodes.at(i);
+            }
+            else
+            {
+                float tmp_v;
+                if (this->child_nodes.at(i).n)
+                {
+                    tmp_v = -this->child_nodes.at(i).w / this->child_nodes.at(i).n;
+                }
+                else
+                {
+                    tmp_v = 0;
+                }
+                tmp_v += (C_PUCT * this->child_nodes.at(i).p * sqrt(t) / (1 + this->child_nodes.at(i).n));
+                pucb_values.push_back(tmp_v);
+            }
+        }
+        int argmax_i = *max_element(pucb_values.begin(), pucb_values.end());
+        return this->child_nodes.at(argmax_i);
+    }
+};
+
 vector<float> boltzman(vector<float> xs, float temperature)
 {
     int len_xs = xs.size();
@@ -47,105 +147,6 @@ vector<float> boltzman(vector<float> xs, float temperature)
 
 vector<float> pv_mcts_scores(auto model, State state, float temperature)
 {
-    class Node
-    {
-    private:
-    public:
-        State state;
-        int p = 0;
-        int w = 0;
-        int n = 0;
-        vector<Node> child_nodes;
-        Node(State s, int np)
-        {
-            state = s;
-            p = np;
-            vector<Node> child_nodes;
-        }
-
-        vector<float> nodes_to_scores(vector<Node> nodes)
-        {
-            vector<float> scores;
-            for (int i = 0; i < nodes.size(); i++)
-            {
-                scores.push_back(nodes.at(i).n);
-            }
-            return scores;
-        }
-
-        float evaluate(auto model)
-        {
-            float value = 0;
-            if (this->state.is_done())
-            {
-                if (this->state.is_lose())
-                {
-                    value = -1;
-                }
-                else
-                {
-                    value = 0;
-                }
-                this->w += value;
-                this->n += 1;
-                return value;
-            }
-
-            if (this->child_nodes.empty())
-            {
-                auto result = predict(model, this->state);
-                vector<int> policies = result.p;
-                int value = result.v;
-                this->w += value;
-                this->n += 1;
-
-                for (int i = 0; i < policies.size(); i++)
-                {
-                    this->child_nodes.push_back(Node(this->state.next(this->state.legal_actions().at(i)), policies.at(i)));
-                }
-                return value;
-            }
-            else
-            {
-                value -= this->next_child_node().evaluate(model);
-                this->w += value;
-                this->n += 1;
-                return value;
-            }
-        }
-
-        Node next_child_node()
-        {
-            float C_PUCT = 1.0;
-            vector<float> scores = nodes_to_scores(this->child_nodes);
-            float t = accumulate(scores.begin(), scores.end(), 0.0);
-
-            vector<float> pucb_values;
-            for (int i = 0; i < this->child_nodes.size(); i++)
-            {
-                if (this->child_nodes.at(i).n == 0)
-                {
-                    return this->child_nodes.at(i);
-                }
-                else
-                {
-                    float tmp_v;
-                    if (this->child_nodes.at(i).n)
-                    {
-                        tmp_v = -this->child_nodes.at(i).w / this->child_nodes.at(i).n;
-                    }
-                    else
-                    {
-                        tmp_v = 0;
-                    }
-                    tmp_v += (C_PUCT * this->child_nodes.at(i).p * sqrt(t) / (1 + this->child_nodes.at(i).n));
-                    pucb_values.push_back(tmp_v);
-                }
-            }
-            int argmax_i = *max_element(pucb_values.begin(), pucb_values.end());
-            return this->child_nodes.at(argmax_i);
-        }
-    };
     Node root_node = Node(state, 0);
     for (int i = 0; i < PV_EVALUATE_COUNT; i++)
     {
