a4601f8e6734940c89a1b5f429735de662b6f913
tomru112345 <kazt23@gmail.com>
2022-11-13 22:49:08 +0900

    fix statebind36

--- reversi/mctsbind.cpp
+++ reversi/mctsbind.cpp
@@ -4,10 +4,19 @@
 
 using namespace std;
 
-PYBIND11_MODULE(cppMCTS, m)
-{
+// PYBIND11_MODULE(cppMCTS, m)
+// {
+//     m.def("predict", &predict);
+//     m.def("boltzman", &boltzman);
+//     m.def("pv_mcts_scores", &pv_mcts_scores);
+//     m.def("pv_mcts_action", &pv_mcts_action);
+// }
+
+PYBIND11_PLUGIN(cppMCTS) {
+    pybind11::module m("cppMCTS", "mylibs made by pybind11");
     m.def("predict", &predict);
     m.def("boltzman", &boltzman);
     m.def("pv_mcts_scores", &pv_mcts_scores);
     m.def("pv_mcts_action", &pv_mcts_action);
+    return m.ptr();
 }
