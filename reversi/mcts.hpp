d9af9e08f0905c8f70845527ea0e24d5bd4bca4d
tomru112345 <kazt23@gmail.com>
2022-11-14 15:19:36 +0900

    fix statebind49

--- reversi/mcts.hpp
+++ reversi/mcts.hpp
@@ -121,63 +121,53 @@ public:
     }
 };
 
-class MCTS
+vector<float> boltzman(vector<float> xs, float temperature)
 {
-public:
-    float temperature;
-    MCTS(float temperature)
+    int len_xs = xs.size();
+    float sum_xs = 0;
+    for (int i = 0; i < len_xs; i++)
     {
-        this->temperature = temperature;
+        float x = xs[i];
+        xs[i] = pow(x, 1 / temperature);
+        sum_xs += xs[i];
     }
-    vector<float> boltzman(vector<float> xs)
+    vector<float> new_xs(len_xs);
+    for (int i = 0; i < len_xs; i++)
     {
-        int len_xs = xs.size();
-        float sum_xs = 0;
-        for (int i = 0; i < len_xs; i++)
-        {
-            float x = xs[i];
-            xs[i] = pow(x, 1 / this->temperature);
-            sum_xs += xs[i];
-        }
-        vector<float> new_xs(len_xs);
-        for (int i = 0; i < len_xs; i++)
-        {
-            new_xs[i] = 0.0;
-            new_xs[i] = xs[i] / sum_xs;
-        }
-        return new_xs;
+        new_xs[i] = 0.0;
+        new_xs[i] = xs[i] / sum_xs;
     }
+    return new_xs;
+}
 
-    vector<float> pv_mcts_scores(auto model, State state)
+vector<float> pv_mcts_scores(auto model, State state, float temperature)
+{
+    Node root_node = Node(state, 0);
+    for (int i = 0; i < PV_EVALUATE_COUNT; i++)
     {
-        Node root_node = Node(state, 0);
-        for (int i = 0; i < PV_EVALUATE_COUNT; i++)
-        {
-            root_node.evaluate(model);
-        }
-
-        vector<float> scores = root_node.nodes_to_scores(root_node.child_nodes);
-        if (this->temperature == 0)
-        {
-            int action = *max_element(scores.begin(), scores.end());
-            scores = vector<float>(scores.size(), 0);
-            scores[action] = 1;
-        }
-        else
-        {
-            scores = boltzman(scores);
-        }
-        return scores;
+        root_node.evaluate(model);
     }
 
-    int pv_mcts_action(auto model, State state)
+    vector<float> scores = root_node.nodes_to_scores(root_node.child_nodes);
+    if (temperature == 0)
     {
-        vector<float> scores = get_scores(model, state);
-        vector<int> leg_ac = state.legal_actions();
-        auto pypre = pybind11::module::import("py_rand_choice");
-        int action = pypre.attr("choice")(leg_ac, scores).cast<int>();
-        return action;
+        int action = *max_element(scores.begin(), scores.end());
+        scores = vector<float>(scores.size(), 0);
+        scores[action] = 1;
     }
-};
+    else
+    {
+        scores = boltzman(scores, temperature);
+    }
+    return scores;
+}
 
+int pv_mcts_action(auto model, State state, float temperature)
+{
+    vector<float> scores = pv_mcts_scores(model, state, temperature);
+    vector<int> leg_ac = state.legal_actions();
+    auto pypre = pybind11::module::import("py_rand_choice");
+    int action = pypre.attr("choice")(leg_ac, scores).cast<int>();
+    return action;
+}
 #endif
--- reversi/mctsbind.cpp
+++ reversi/mctsbind.cpp
@@ -4,19 +4,13 @@
 
 using namespace std;
 
-// PYBIND11_MODULE(cppMCTS, m)
-// {
-//     m.def("predict", &predict);
-//     m.def("boltzman", &boltzman);
-//     m.def("pv_mcts_scores", &pv_mcts_scores);
-//     m.def("pv_mcts_action", &pv_mcts_action);
-// }
-
 PYBIND11_MODULE(cppMCTS, m)
 {
+    m.def("boltzman", &boltzman);
+    m.def("pv_mcts_scores", &pv_mcts_scores);
     pybind11::class_<MCTS>(m, "MCTS")
         .def(pybind11::init<float>())
-        .def("boltzman", &MCTS::boltzman)
-        .def("pv_mcts_scores", &MCTS::pv_mcts_scores);
+        
+        
         // .def("pv_mcts_action", &MCTS::pv_mcts_action);
 }
