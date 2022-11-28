2d79f04afceba3d191d88c72794da8a71cf1e51a
tamura_kazuma <kazuma.tamura.6n@stu.hosei.ac.jp>
2022-11-29 04:56:14 +0900

    add policy\iter

--- reversi/policy_iteration.py
+++ reversi/policy_iteration.py
@@ -3,17 +3,44 @@ from collections import defaultdict
 import numpy as np
 
 
-pi = defaultdict(State)
-V = defaultdict(lambda: 0)
-gamma = 0.9
+def first_player_value(state, next_state):
+    """先手プレイヤーの価値"""
+    # 1:先手勝利, -1:先手敗北, 0:引き分け
+    if not state.is_lose():
+        if next_state.is_lose():
+            return -1.0 if next_state.is_first_player() else 1.0
+    return 0.0
 
 
-def eval_onestep(pi, V, state: State, gamma: float = 0.9):
-    if state.is_done():
-        V.append(0)  # 終了したときの価値関数は常に 0
-        return V
+class Agent:
+    def __init__(self, state: State) -> None:
+        """初期化"""
+        self.gamma = 0.9
+        self.agent_state = [state.pieces, state.enemy_pieces, state.ratio_box]
+        self.pi = defaultdict(State)
+        self.V = defaultdict(lambda: 0)
+        self.flg_first_player = state.is_first_player()
 
-    pass
+    def set_agent_state(self, state: State):
+        """エージェントの盤面状態の更新"""
+        self.agent_state = [state.pieces, state.enemy_pieces, state.ratio_box]
+
+    def eval_onestep(self, state: State):
+        self.set_agent_state(state=state)
+        if state.is_done():
+            self.V[self.agent_state] = 0  # 終了したときの価値関数は常に 0
+            return self.V
+
+        action_probs = self.pi[self.agent_state]
+        new_V = 0
+
+        for action, action_prob in action_probs.items():
+            next_state: State = state.next(action)
+            reward = first_player_value(state, next_state)
+            new_V += action_prob * (reward + self.gamma * self.V[next_state])
+
+        self.V[state] = new_V
+        pass
 
 
 def policy_eval(pi, V, state: State, gamma: float = 0.9, threshold: float = 0.001):
