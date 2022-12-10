55262ba16fe652cd9465b9da9ff5e8f7a7c0c90e
tomru112345 <kazt23@gmail.com>
2022-12-10 18:35:22 +0900

    remove state list and make state dict

--- reversi/value_iteration.py
+++ reversi/value_iteration.py
@@ -1,5 +1,4 @@
-from game import State
-# from cppState import State
+from game import State, random_action
 from settings import default_ratio_box
 import sys
 import numpy as np
@@ -8,156 +7,84 @@ sys.setrecursionlimit(10 ** 9)
 
 
 def reward(state: State):
-    if state.piece_count(state.pieces) == state.piece_count(state.enemy_pieces):
+    if state.is_done():
+        if state.piece_count(state.pieces) > state.piece_count(state.enemy_pieces):
+            return 1
+        elif state.piece_count(state.pieces) < state.piece_count(state.enemy_pieces):
+            return -1
+    else:
         return 0
-    elif state.piece_count(state.pieces) < state.piece_count(state.enemy_pieces):
-        return 1
-    else:
-        return -1
-
-
-def check_fin(state: State):
-    if state.piece_count(state.pieces) + state.piece_count(state.enemy_pieces) == 16:
-        return True
-    elif state.piece_count(state.pieces) == 0:
-        return True
-    elif state.piece_count(state.enemy_pieces) == 0:
-        return True
-    elif state.legal_actions() == [16]:
-        next_state = state.next(16)
-        if next_state.legal_actions() == [16]:
-            return True
-        else:
-            return False
-    else:
-        return False
 
 
 cnt = -1
 
-state_4 = []
-state_5 = []
-state_6 = []
-state_7 = []
-state_8 = []
-state_9 = []
-state_10 = []
-state_11 = []
-state_12 = []
-state_13 = []
-state_14 = []
-state_15 = []
-
 all_board = [None] * 63665
-fin_flg_l = [False] * 63665
-all_board_d = {}
+fin_flgs = [False] * 63665
+board_idx_dict = {}
+state_d = {4: [], 5: [], 6: [], 7: [], 8: [], 9: [],
+           10: [], 11: [], 12: [], 13: [], 14: [], 15: []}
 
 
-def search(state: State, turn: int):
-    global all_board, all_board_d, cnt, fin_flg_l, state_15, state_14, state_13, state_12, state_11, state_10, state_9, state_8, state_7, state_6, state_5, state_4
+def search(state: State):
+    global all_board, board_idx_dict, cnt, fin_flgs
     if not (tuple(state.pieces), tuple(
-            state.enemy_pieces), state.depth % 2) in all_board_d:
+            state.enemy_pieces), state.depth % 2) in board_idx_dict:
         cnt += 1
         print('\rinit {:,} / {:,}'.format(cnt + 1, 63665), end='')
-        all_board_d[(tuple(state.pieces), tuple(
+        board_idx_dict[(tuple(state.pieces), tuple(
             state.enemy_pieces), state.depth % 2)] = cnt
         all_board[cnt] = (state.pieces, state.enemy_pieces, state.depth % 2)
         piece_cnt = state.piece_count(state.pieces) + \
             state.piece_count(state.enemy_pieces)
-        if piece_cnt == 4:
-            state_4.append((tuple(state.pieces), tuple(
+
+        if piece_cnt in state_d:
+            tmp_l = state_d[piece_cnt]
+            tmp_l.append((tuple(state.pieces), tuple(
                 state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 5:
-            state_5.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 6:
-            state_6.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 7:
-            state_7.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 8:
-            state_8.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 9:
-            state_9.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 10:
-            state_10.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 11:
-            state_11.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 12:
-            state_12.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 13:
-            state_13.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 14:
-            state_14.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 15:
-            state_15.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
+            state_d[piece_cnt] = tmp_l
 
         if state.is_done():
-            fin_flg_l[cnt] = True
+            fin_flgs[cnt] = True
             return
         else:
             for action in state.legal_actions():
                 next_state = state.next(action=action)
-                search(next_state, turn+1)
+                search(next_state)
 
 
 def value_iter_onestep():
-    global all_board_d, cnt, fin_flg_l, state_15, state_14, state_13, state_12, state_11, state_10, state_9, state_8, state_7, state_6, state_5, state_4
+    global board_idx_dict, cnt, fin_flgs
     V = [0] * 63665
     state = State()
-    search(state=state, turn=0)
+    search(state=state)
     print()
-    states_num_l = [
-        tuple(state_15),
-        tuple(state_14),
-        tuple(state_13),
-        tuple(state_12),
-        tuple(state_11),
-        tuple(state_10),
-        tuple(state_9),
-        tuple(state_8),
-        tuple(state_7),
-        tuple(state_6),
-        tuple(state_5),
-        tuple(state_4),
-    ]
     del state
-    del state_15, state_14, state_13, state_12, state_11, state_10, state_9, state_8, state_7, state_6, state_5, state_4
 
     # # 価値関数の設定
     cnt = 0
     stage = 15
-    for i in range(12):
-        for states_num in states_num_l[i]:
+    for i in reversed(range(4, 16)):
+        for state_idx in state_d[i]:
             print(
                 '\rvalue_iter_onestep {:,} / {:,}'.format(cnt + 1, 58613), end='')
             cnt += 1
-            if fin_flg_l[all_board_d[states_num]]:
+            if fin_flgs[board_idx_dict[state_idx]]:
                 continue
             else:
                 action_values = []
-                pieces, enemy_pieces, depth = all_board[all_board_d[states_num]]
-                state: State = State(pieces, enemy_pieces,
-                                     default_ratio_box, depth % 2)
+                pieces, enemy_pieces, depth = all_board[board_idx_dict[state_idx]]
+                state = State(pieces, enemy_pieces,
+                              default_ratio_box, depth % 2)
                 for action in state.legal_actions():
                     next_state = state.next(action)
-                    na = all_board_d[(tuple(next_state.pieces), tuple(
+                    na = board_idx_dict[(tuple(next_state.pieces), tuple(
                         next_state.enemy_pieces), next_state.depth % 2)]
                     r = 0
-                    if fin_flg_l[na]:
+                    if fin_flgs[na]:
                         r = reward(state)
                     v = r + (-1) * V[na]
                     action_values.append(v)
-                V[all_board_d[states_num]] = max(action_values)
+                V[board_idx_dict[state_idx]] = max(action_values)
                 del action_values, state
         stage -= 1
     print()
@@ -175,15 +102,15 @@ def argmax(d: dict):
 
 
 def greedy_policy(V):
-    global all_board, all_board_d
+    global all_board, board_idx_dict
     pi = [0] * 63665
     # # 価値関数の設定
     for i in range(63665):
         print(
             '\rgreedy_policy {:,} / {:,}'.format(i + 1, 63665), end='')
         pieces, enemy_pieces, depth = all_board[i]
-        state: State = State(pieces, enemy_pieces,
-                             default_ratio_box, depth % 2)
+        state = State(pieces, enemy_pieces,
+                      default_ratio_box, depth % 2)
         len_tmp = len(state.legal_actions())
         action_probs = [0.0] * len_tmp
         if state.is_done():
@@ -192,10 +119,10 @@ def greedy_policy(V):
             action_values = {}
             for action in state.legal_actions():
                 next_state = state.next(action)
-                na = all_board_d[(tuple(next_state.pieces), tuple(
+                na = board_idx_dict[(tuple(next_state.pieces), tuple(
                     next_state.enemy_pieces), next_state.depth % 2)]
                 r = 0
-                if fin_flg_l[na]:
+                if fin_flgs[na]:
                     r = reward(state)
                 v = r + (-1) * V[na]
                 action_values[action] = v
@@ -206,29 +133,50 @@ def greedy_policy(V):
     return pi
 
 
-def main():
-    global all_board_d
+def guess():
+    global board_idx_dict
     V = value_iter_onestep()
     pi = greedy_policy(V)
-    state = State()
-    # ゲーム終了までループ
-    while True:
-        # ゲーム終了時
-        if state.is_done():
-            break
+    return pi
 
-        # 行動の取得
-        index_state = all_board_d[(tuple(state.pieces), tuple(
-            state.enemy_pieces), state.depth % 2)]
-        action_probs = list(pi[index_state])
-        action = np.random.choice(state.legal_actions(), p=action_probs)
-        # 次の状態の取得
-        state = state.next(action)
 
-        # 文字列表示
-        print(state)
+def play(pi, n):
+    black_win = 0
+    white_win = 0
 
+    for _ in range(n):
+        state = State()
+        # ゲーム終了までループ
+        while True:
+            # ゲーム終了時
+            if state.is_done():
+                if state.is_first_player():
+                    if state.piece_count(state.pieces) > state.piece_count(state.enemy_pieces):
+                        black_win += 1
+                    elif state.piece_count(state.pieces) < state.piece_count(state.enemy_pieces):
+                        white_win += 1
+                else:
+                    if state.piece_count(state.pieces) > state.piece_count(state.enemy_pieces):
+                        white_win += 1
+                    elif state.piece_count(state.pieces) < state.piece_count(state.enemy_pieces):
+                        black_win += 1
+                break
+
+            # 行動の取得
+            if not state.is_first_player():
+                action = np.random.choice(state.legal_actions(), p=list(pi[board_idx_dict[(tuple(state.pieces), tuple(
+                    state.enemy_pieces), state.depth % 2)]]))
+            else:
+                action = random_action(state)
+            # 次の状態の取得
+            state = state.next(action)
+
+            # 文字列表示
+            print(state)
+
+    print(f"{black_win} vs {white_win}")
 
     # 動作確認
 if __name__ == '__main__':
-    main()
+    pi = guess()
+    # play(pi, 100)
