dc0f6bac3eb24c948bc1a69e4ca6d4e033e85d9d
tamura_kazuma <kazuma.tamura.6n@stu.hosei.ac.jp>
2022-12-15 13:57:15 +0900

    fix cd dir

--- install.bat
+++ install.bat
@@ -1,3 +1,6 @@
 python -m pip install -r requirements.txt
-python reversi\mypackage\cppState\setup.py install
-python reversi\mypackage\cppNode\setup.py install
+cd reversi\mypackage\cppState
+python setup.py install
+cd ..\cppNode
+python setup.py install
+cd ..\..\
