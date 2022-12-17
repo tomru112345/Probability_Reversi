cacffe7b1d207e1628469ae45ac0b0c545b4aebc
tomru112345 <kazt23@gmail.com>
2022-12-17 21:32:21 +0900

    write human data

--- reversi/debug.py
+++ reversi/debug.py
@@ -1,161 +1,22 @@
 from game import State
 # from cppState import State
 from settings import default_ratio_box
+from datetime import datetime
+from pathlib import Path
+import sys
+import pickle
+import os
+import itertools
+import gc
+
+def load_data():
+    """学習データの読み込み"""
+    history_path = sorted(
+        Path(f'./human_play/').glob('*.history'))[-1]
+    with history_path.open(mode='rb') as f:
+        return pickle.load(f)
 
 
-def reward(state: State):
-    if state.piece_count(state.pieces) == state.piece_count(state.enemy_pieces):
-        return 0
-    elif state.piece_count(state.pieces) > state.piece_count(state.enemy_pieces):
-        return 1
-    else:
-        return -1
-
-
-def check_fin(state: State):
-    if state.piece_count(state.pieces) + state.piece_count(state.enemy_pieces) == 16:
-        return True
-    elif state.piece_count(state.pieces) == 0:
-        return True
-    elif state.piece_count(state.enemy_pieces) == 0:
-        return True
-    elif state.legal_actions() == [16]:
-        next_state = state.next(16)
-        if next_state.legal_actions() == [16]:
-            return True
-        else:
-            return False
-    else:
-        return False
-
-
-cnt = -1
-
-adress_4 = []
-adress_5 = []
-adress_6 = []
-adress_7 = []
-adress_8 = []
-adress_9 = []
-adress_10 = []
-adress_11 = []
-adress_12 = []
-adress_13 = []
-adress_14 = []
-adress_15 = []
-
-all_board = [None] * 63665
-fin_flg_l = [False] * 63665
-all_board_d = {}
-
-
-def search(state: State, turn: int):
-    global all_board_d, cnt, fin_flg_l, adress_15, adress_14, adress_13, adress_12, adress_11, adress_10, adress_9, adress_8, adress_7, adress_6, adress_5, adress_4
-    if not (tuple(state.pieces), tuple(
-            state.enemy_pieces), state.depth % 2) in all_board_d:
-        cnt += 1
-        print('\rinit {:,} / {:,}'.format(cnt + 1, 63665), end='')
-        all_board_d[(tuple(state.pieces), tuple(
-            state.enemy_pieces), state.depth % 2)] = cnt
-        all_board[cnt] = (state.pieces, state.enemy_pieces, state.depth % 2)
-        piece_cnt = state.piece_count(state.pieces) + \
-            state.piece_count(state.enemy_pieces)
-        if piece_cnt == 4:
-            adress_4.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 5:
-            adress_5.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 6:
-            adress_6.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 7:
-            adress_7.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 8:
-            adress_8.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 9:
-            adress_9.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 10:
-            adress_10.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 11:
-            adress_11.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 12:
-            adress_12.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 13:
-            adress_13.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 14:
-            adress_14.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        elif piece_cnt == 15:
-            adress_15.append((tuple(state.pieces), tuple(
-                state.enemy_pieces), state.depth % 2))
-        if state.is_done():
-            fin_flg_l[cnt] = True
-            return
-        else:
-            for action in state.legal_actions():
-                next_state = state.next(action=action)
-                search(next_state, turn+1)
-
-
-def value_iter_onestep():
-    global all_board_d, cnt, fin_flg_l, adress_15, adress_14, adress_13, adress_12, adress_11, adress_10, adress_9, adress_8, adress_7, adress_6, adress_5, adress_4
-    V = [0] * 63665
-    state = State()
-    search(state=state, turn=0)
-    print()
-    address = [
-        tuple(adress_15),
-        tuple(adress_14),
-        tuple(adress_13),
-        tuple(adress_12),
-        tuple(adress_11),
-        tuple(adress_10),
-        tuple(adress_9),
-        tuple(adress_8),
-        tuple(adress_7),
-        tuple(adress_6),
-        tuple(adress_5),
-        tuple(adress_4),
-    ]
-    del state
-    del adress_15, adress_14, adress_13, adress_12, adress_11, adress_10, adress_9, adress_8, adress_7, adress_6, adress_5, adress_4
-
-    # # 価値関数の設定
-    cnt = 0
-    stage = 15
-    for i in range(12):
-        for a in address[i]:
-            print(
-                '\rvalue_iter_onestep {:,} / {:,}: stage: {}'.format(cnt + 1, 58613, stage), end='')
-            cnt += 1
-            if fin_flg_l[all_board_d[a]]:
-                continue
-            else:
-                action_values = []
-                pieces, enemy_pieces, depth = all_board[all_board_d[a]]
-                state: State = State(pieces, enemy_pieces,
-                                     default_ratio_box, depth % 2)
-                for action in state.legal_actions():
-                    next_state = state.next(action)
-                    na = all_board_d[(tuple(next_state.pieces), tuple(
-                        next_state.enemy_pieces), next_state.depth % 2)]
-                    r = 0
-                    if fin_flg_l[na]:
-                        r = reward(state)
-                    v = r + (-1) * V[na]
-                    action_values.append(v)
-                V[all_board_d[a]] = max(action_values)
-                del action_values, state
-        stage -= 1
-    return V
-
-
-value_iter_onestep()
+l = load_data()
+print(l[0])
+print(l[1])
--- reversi/human_play.py
+++ reversi/human_play.py
@@ -11,8 +11,10 @@ from keras.models import load_model
 from pathlib import Path
 from threading import Thread
 import tkinter as tk
+import pickle
 from settings import SQUARE, default_ratio_box
 import os
+from datetime import datetime
 
 # tensorflow の warning の設定
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -27,6 +29,7 @@ class GameUI(tk.Frame):
         """初期化"""
         tk.Frame.__init__(self, master)
         self.master.title('確率リバーシ')
+        self.master.resizable(width=False, height=False)
 
         # ゲーム状態の生成
         self.state = State(default_ratio_box)
@@ -35,9 +38,14 @@ class GameUI(tk.Frame):
         self.before_action = None
         self.model = model
 
+        self.history = []
+        self.human_match_result = [0] * 10
+
+        self.play_cnt = 1
+
         # キャンバスの生成
-        self.c = tk.Canvas(self, width=SQUARE * 40,
-                           height=(SQUARE + 1) * 40, highlightthickness=0)
+        self.c = tk.Canvas(self, width=SQUARE * 100,
+                           height=(SQUARE + 1) * 100, highlightthickness=0)
         self.c.bind('<Button-1>', self.turn_of_human)
         self.c.pack()
 
@@ -63,8 +71,21 @@ class GameUI(tk.Frame):
     def turn_of_human(self, event):
         """人間のターン"""
         # ゲーム終了時
+        self.history.append([self.state.get_pieces(), self.state.get_enemy_pieces(), self.state.get_ratio_box(), self.state.get_depth()])
         if self.state.is_done():
+            if self.state.is_lose():
+                self.human_match_result[self.play_cnt - 1] = -1
+            elif self.state.is_draw():
+                self.human_match_result[self.play_cnt - 1] = 0
+            else:
+                self.human_match_result[self.play_cnt - 1] = 1
             self.state = State(default_ratio_box)
+            self.play_cnt += 1
+            if self.play_cnt == 6:
+                self.first_ai = True
+            if self.play_cnt == 11:
+                self.write_data()
+                exit()
             self.reset()
             self.on_draw()
             return
@@ -76,8 +97,8 @@ class GameUI(tk.Frame):
             return
 
         # クリック位置を行動に変換
-        x = int(event.x/40)
-        y = int(event.y/40)
+        x = int(event.x/100)
+        y = int(event.y/100)
         if x < 0 or (SQUARE - 1) < x or y < 0 or (SQUARE - 1) < y:  # 範囲外
             return
         action = x + y * SQUARE
@@ -102,6 +123,7 @@ class GameUI(tk.Frame):
 
     def turn_of_ai(self):
         """AIのターン"""
+        self.history.append([self.state.get_pieces(), self.state.get_enemy_pieces(), self.state.get_ratio_box(), self.state.get_depth()])
         # ゲーム終了時
         if self.state.is_done():
             return
@@ -120,19 +142,52 @@ class GameUI(tk.Frame):
 
     def draw_piece(self, index, first_player):
         """石の描画"""
-        x = (index % SQUARE)*40+5
-        y = int(index/SQUARE)*40+5
+        x = (index % SQUARE)*100+5
+        y = int(index/SQUARE)*100+5
         if first_player:
-            self.c.create_oval(x, y, x+30, y+30, width=1.0,
+            self.c.create_oval(x, y, x+100 - 10, y+100 - 10, width=1.0,
                                outline='#000000', fill='#222222')
         else:
-            self.c.create_oval(x, y, x+30, y+30, width=1.0,
+            self.c.create_oval(x, y, x+100 - 10, y+100 - 10, width=1.0,
                                outline='#000000', fill='#FFFFFF')
+
+    def draw_result(self):
+        x = int((SQUARE * 100) / 2)
+        y = int((SQUARE * 100) / 2)
+        if self.state.is_first_player():
+            black_pieces = self.state.piece_count(self.state.get_pieces())
+            white_pieces = self.state.piece_count(
+                self.state.get_enemy_pieces())
+        else:
+            black_pieces = self.state.piece_count(
+                self.state.get_enemy_pieces())
+            white_pieces = self.state.piece_count(self.state.get_pieces())
+
+        if not self.first_ai:
+            if black_pieces > white_pieces:
+                self.c.create_text(
+                    x, y, text=f'勝利', fill="#FFFFFF", font=('Yu Gothic UI', 100), anchor="center")
+            elif black_pieces == white_pieces:
+                self.c.create_text(
+                    x, y, text=f'引き分け', fill="#FFFFFF", font=('Yu Gothic UI', 100), anchor="center")
+            else:
+                self.c.create_text(
+                    x, y, text=f'敗北', fill="#FFFFFF", font=('Yu Gothic UI', 100), anchor="center")
+        else:
+            if black_pieces < white_pieces:
+                self.c.create_text(
+                    x, y, text=f'勝利', fill="#FFFFFF", font=('Yu Gothic UI', 100), anchor="center")
+            elif black_pieces == white_pieces:
+                self.c.create_text(
+                    x, y, text=f'引き分け', fill="#FFFFFF", font=('Yu Gothic UI', 100), anchor="center")
+            else:
+                self.c.create_text(
+                    x, y, text=f'敗北', fill="#FFFFFF", font=('Yu Gothic UI', 100), anchor="center")
 
     def draw_ratio(self, index):
         """確率をマス目に表示"""
-        x = (index % SQUARE)*40+20
-        y = int(index/SQUARE)*40+20
+        x = (index % SQUARE)*100+50
+        y = int(index/SQUARE)*100+50
         self.c.create_text(
             x, y, text=str(self.state.get_ratio_box()[index]), fill="#FF0461", font=('Yu Gothic UI', 15), anchor="center")
 
@@ -144,18 +199,18 @@ class GameUI(tk.Frame):
                 x = (index % SQUARE)
                 y = int(index/SQUARE)
                 self.c.create_rectangle(
-                    x * 40, y * 40, (x + 1) * 40, (y + 1) * 40, fill='#FF9900')
+                    x * 100, y * 100, (x + 1) * 100, (y + 1) * 100, fill='#FF9900')
 
     def draw_status(self):
         """現状の石の個数の表示"""
-        x = int((SQUARE * 40) / 2)
-        y = SQUARE * 40+20
+        x = int((SQUARE * 100) / 2)
+        y = SQUARE * 100+50
         if self.state.is_first_player():
             black_pieces = self.state.piece_count(self.state.get_pieces())
             white_pieces = self.state.piece_count(
                 self.state.get_enemy_pieces())
             self.c.create_rectangle(
-                0, SQUARE * 40, SQUARE * 40, (SQUARE + 1) * 40, fill='#222222')
+                0, SQUARE * 100, SQUARE * 100, (SQUARE + 1) * 100, fill='#222222')
             self.c.create_text(
                 x, y, text=f'{black_pieces} : {white_pieces}', fill="#FFFFFF", font=('Yu Gothic UI', 18), anchor="center")
         else:
@@ -163,30 +218,53 @@ class GameUI(tk.Frame):
                 self.state.get_enemy_pieces())
             white_pieces = self.state.piece_count(self.state.get_pieces())
             self.c.create_rectangle(
-                0, SQUARE * 40, SQUARE * 40, (SQUARE + 1) * 40, fill='#FFFFFF')
+                0, SQUARE * 100, SQUARE * 100, (SQUARE + 1) * 100, fill='#FFFFFF')
             self.c.create_text(
                 x, y, text=f'{black_pieces} : {white_pieces}', fill="#222222", font=('Yu Gothic UI', 18), anchor="center")
 
+    def draw_turn_player(self):
+        """現状の石の個数の表示"""
+        x = int((SQUARE * 100) / 2)
+        y = SQUARE * 100+50
+        turn_player_msg = ["あなた", "AI"]
+        if self.first_ai:
+            turn_player_msg.reverse()
+        if self.state.is_first_player():
+            self.c.create_rectangle(
+                0, SQUARE * 100, SQUARE * 100, (SQUARE + 1) * 100, fill='#222222')
+            self.c.create_text(
+                x, y, text=f'{turn_player_msg[0]}', fill="#FFFFFF", font=('Yu Gothic UI', 18), anchor="center")
+        else:
+            self.c.create_rectangle(
+                0, SQUARE * 100, SQUARE * 100, (SQUARE + 1) * 100, fill='#FFFFFF')
+            self.c.create_text(
+                x, y, text=f'{turn_player_msg[1]}', fill="#222222", font=('Yu Gothic UI', 18), anchor="center")
+
     def draw_select_icon(self, index):
         """どこを選択したか表示"""
-        x = (index % SQUARE)*40+5
-        y = int(index/SQUARE)*40+5
-        self.c.create_oval(x, y, x+30, y+30, width=5.0,
+        x = (index % SQUARE)*100+5
+        y = int(index/SQUARE)*100+5
+        self.c.create_oval(x, y, x+100 - 10, y+100 - 10, width=5.0,
                            outline='#FF0461')
+
+    def push_btn_first_player(self, event):
+        self.game_status = 1
+        self.first_ai = False
+        self.on_draw()
 
     def on_draw(self):
         """描画の更新"""
         self.c.delete('all')
         self.c.create_rectangle(
-            0, 0, SQUARE * 40, SQUARE * 40, width=0.0, fill='#00DD00')
+            0, 0, SQUARE * 100, SQUARE * 100, width=0.0, fill='#00DD00')
 
         # 合法手を描画
         self.draw_legal_action()
 
         for i in range(1, SQUARE + 2):
-            self.c.create_line(0, i*40, SQUARE * 40, i*40,
+            self.c.create_line(0, i*100, SQUARE * 100, i*100,
                                width=1.0, fill='#000000')
-            self.c.create_line(i*40, 0, i*40, SQUARE * 40,
+            self.c.create_line(i*100, 0, i*100, SQUARE * 100,
                                width=1.0, fill='#000000')
         for i in range(SQUARE * SQUARE):
             if self.state.get_pieces()[i] == 1:
@@ -200,7 +278,17 @@ class GameUI(tk.Frame):
             if self.before_action == i:
                 self.draw_select_icon(i)
 
-        self.draw_status()
+        self.draw_turn_player()
+
+    def write_data(self):
+        """学習データの保存"""
+        now = datetime.now()
+        os.makedirs(f'./human_play/', exist_ok=True)  # フォルダがない時は生成
+        path = './human_play/{:04}{:02}{:02}{:02}{:02}{:02}.history'.format(
+            now.year, now.month, now.day, now.hour, now.minute, now.second)
+        result = [self.history, self.human_match_result]
+        with open(path, mode='wb') as f:
+            pickle.dump(result, f)
 
 
 # 動作確認
